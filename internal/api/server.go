3cfd742bbcce45a19a833acea63ebcf8ff2e8ad6
MaximIvanchenko18 <maximovich2003@gmail.com>
2023-09-27 12:10:03 +0000

    Final

--- internal/api/server.go
+++ internal/api/server.go
@@ -16,23 +16,23 @@ type cargo struct {
 	Photo       string  `json:"photo src"`
 	Category    string  `json:"category"`
 	Name        string  `json:"name"`
-	Price       int32   `json:"price"`
-	Unit        string  `json:"unit"`
+	Price       int32   `json:"price"`    // Rubles
+	Weight      float32 `json:"weight"`   // kg
+	Capacity    float32 `json:"capacity"` // m^3
 	Description string  `json:"description"`
-	Rating      float32 `json:"rating"`
 }
 
 var goods = []cargo{
-	{EnglishName: "oxygen", Photo: "/image/oxygen.jpg", Category: "Кислород", Name: "Кислород", Price: 390, Unit: "1 литр", Description: "Кислород в баллонах под высоким давлением", Rating: 5.0},
-	{EnglishName: "water", Photo: "/image/water.jpg", Category: "Напитки", Name: "Вода", Price: 72, Unit: "1 литр", Description: "Вода родниковая очищенная", Rating: 4.9},
-	{EnglishName: "tea", Photo: "/image/tea.jpg", Category: "Напитки", Name: "Чай черный", Price: 750, Unit: "3 грамма", Description: "Чай черный цейлонский без сахара в специализированном пакете", Rating: 5.0},
-	{EnglishName: "coffee", Photo: "/image/coffee.jpg", Category: "Напитки", Name: "Кофе с молоком и сахаром", Price: 1140, Unit: "30 граммов", Description: "Кофе натуральный Arabica, натуральное молоко, сахар-песок", Rating: 4.8},
-	{EnglishName: "kisel", Photo: "/image/kisel.jpg", Category: "Напитки", Name: "Кисель вишневый", Price: 25, Unit: "120 граммов", Description: "Сахар-песок, крахмал, сок вишневый концентрированный, витаминная смесь, кислота лимонная", Rating: 4.2},
-	{EnglishName: "bread", Photo: "/image/bread.jpg", Category: "Еда", Name: "Хлеб пшеничный", Price: 380, Unit: "30 граммов", Description: "Мука пшеничная, вода, маргарин, сахар, дрожжи, соль, молоко сухое", Rating: 5.0},
-	{EnglishName: "meat_and_grecha", Photo: "/image/meat_grecha.jpg", Category: "Еда", Name: "Каша гречневая с мясом", Price: 15, Unit: "60 граммов", Description: "Крупа гречневая, соль, жир, фарш говяжий сушеный, лук сушеный, аромат говядины", Rating: 4.8},
-	{EnglishName: "borsh", Photo: "/image/borsh.jpg", Category: "Еда", Name: "Борщ с мясом", Price: 1100, Unit: "30 граммов", Description: "Мясо-говядина крупнокусковое, картофель, капуста свежая, свекла, морковь, лук репчатый, корень петрушки, томатная паста, пюре из перца, сахар-песок, масло топленое, соль поваренная, лимонная кислота, лист лавровый, перец черный молотый, бульон", Rating: 4.7},
-	{EnglishName: "biscuits", Photo: "/image/biscuits.jpg", Category: "Еда", Name: "Печенье Восток", Price: 510, Unit: "30 граммов", Description: "Мука высшего сорта, крахмал маисовый, сахарная пудра, инвертный сироп, маргарин, молоко цельное, ванильная пудра, соль, сода, аммоний, эссенция", Rating: 5.0},
-	{EnglishName: "personal", Photo: "/image/personal.jpg", Category: "Личные вещи", Name: "Посылка от родных", Price: 0, Unit: "1 штука", Description: "Личные посылки от родственников и друзей космонавтов", Rating: 5.0},
+	{EnglishName: "oxygen", Photo: "/image/oxygen.jpg", Category: "Кислород", Name: "Кислород", Price: 19500, Weight: 60, Capacity: 0.05, Description: "Кислород в баллонах под высоким давлением"},
+	{EnglishName: "water", Photo: "/image/water.jpg", Category: "Напитки", Name: "Вода", Price: 110, Weight: 0.5, Capacity: 0.0005, Description: "Вода родниковая очищенная"},
+	{EnglishName: "tea", Photo: "/image/tea.jpg", Category: "Напитки", Name: "Чай черный", Price: 750, Weight: 0.003, Capacity: 0.0003, Description: "Чай черный цейлонский без сахара в специализированном пакете"},
+	{EnglishName: "coffee", Photo: "/image/coffee.jpg", Category: "Напитки", Name: "Кофе с молоком и сахаром", Price: 1140, Weight: 0.03, Capacity: 0.0002, Description: "Кофе натуральный Arabica, натуральное молоко, сахар-песок"},
+	{EnglishName: "kisel", Photo: "/image/kisel.jpg", Category: "Напитки", Name: "Кисель вишневый", Price: 25, Weight: 0.12, Capacity: 0.00005, Description: "Сахар-песок, крахмал, сок вишневый концентрированный, витаминная смесь, кислота лимонная"},
+	{EnglishName: "bread", Photo: "/image/bread.jpg", Category: "Еда", Name: "Хлеб пшеничный", Price: 380, Weight: 0.03, Capacity: 0.00015, Description: "Мука пшеничная, вода, маргарин, сахар, дрожжи, соль, молоко сухое"},
+	{EnglishName: "meat_and_grecha", Photo: "/image/meat_grecha.jpg", Category: "Еда", Name: "Каша гречневая с мясом", Price: 15, Weight: 0.06, Capacity: 0.00025, Description: "Крупа гречневая, соль, жир, фарш говяжий сушеный, лук сушеный, аромат говядины"},
+	{EnglishName: "borsh", Photo: "/image/borsh.jpg", Category: "Еда", Name: "Борщ с мясом", Price: 1100, Weight: 0.03, Capacity: 0.0003, Description: "Мясо-говядина крупнокусковое, картофель, капуста свежая, свекла, морковь, лук репчатый, корень петрушки, томатная паста, пюре из перца, сахар-песок, масло топленое, соль поваренная, лимонная кислота, лист лавровый, перец черный молотый, бульон"},
+	{EnglishName: "biscuits", Photo: "/image/biscuits.jpg", Category: "Еда", Name: "Печенье Восток", Price: 510, Weight: 0.03, Capacity: 0.0002, Description: "Мука высшего сорта, крахмал маисовый, сахарная пудра, инвертный сироп, маргарин, молоко цельное, ванильная пудра, соль, сода, аммоний, эссенция"},
+	{EnglishName: "personal", Photo: "/image/personal.jpg", Category: "Личные вещи", Name: "Посылка от родных", Price: 0, Weight: 1, Capacity: 1, Description: "Личные посылки от родственников и друзей космонавтов"},
 }
 
 func StartServer() {
@@ -49,7 +49,7 @@ func StartServer() {
 		lowprice := c.Query("LowPrice")
 		highprice := c.Query("HighPrice")
 		filtered_goods := filterGoods(goods, name, lowprice, highprice)
-		c.HTML(http.StatusOK, "start.tmpl", gin.H{
+		c.HTML(http.StatusOK, "cargo.tmpl", gin.H{
 			"Cargo":      filtered_goods,
 			"searchName": name,
 			"lowPrice":   lowprice,
