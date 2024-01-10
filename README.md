f565fe49edd9155da76b19a57bbde262645cf808
MaximIvanchenko18 <maximovich2003@gmail.com>
2024-01-10 23:48:03 +0300

    final changes

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# InternetApps_5sem
-РИП 1 семестр 3 курс МГТУ им. Баумана
+# Разработка интернет-приложений
+РИП 5 семестр МГТУ им. Баумана
+
+# Тема
+**Грузовые корабли к МКС.**
+Услуги - грузы для доставки на МКС.
+Заявки - полеты грузовых кораблей к МКС.
+
+
+# Backend:
+## Основной сервис
+- [SSR (лаб 1)](https://github.com/MaximIvanchenko18/InternetApps_5sem/tree/ssr)
+- [База данных (лаб 2)](https://github.com/MaximIvanchenko18/InternetApps_5sem/tree/database)
+- [REST-API (лаб 3)](https://github.com/MaximIvanchenko18/InternetApps_5sem/tree/webservice)
+- [Аутентификация и авторизация (лаб 5)](https://github.com/MaximIvanchenko18/InternetApps_5sem/tree/authorization)
+
+## Асинхронный сервис
+- [Асинхронный сервис (лаб 8)](https://github.com/MaximIvanchenko18/InternetApps_5sem_Async/tree/async)
+
+# Frontend:
+- [SPA (лаб 4)](https://github.com/MaximIvanchenko18/InternetApps_5sem_Frontend/tree/spa)
+- [Redux (лаб 6)](https://github.com/MaximIvanchenko18/InternetApps_5sem_Frontend/tree/redux)
+- [Pages](https://maximivanchenko18.github.io/InternetApps_5sem_Frontend/)
+
+# Мобильное приложение:
+- [React Native (лаб 7)](https://github.com/MaximIvanchenko18/InternetApps_5sem_Mobile/tree/nativeapp)
+
+# Домашнее задание:
+- [Интерфейс модератора](https://github.com/MaximIvanchenko18/InternetApps_5sem_Frontend/tree/moderator)
--- cargo_iss_5.sql
+++ cargo_iss_5.sql
@@ -17,12 +17,13 @@ CREATE TABLE "public"."cargos" (
     CONSTRAINT "cargos_pkey" PRIMARY KEY ("uuid")
 ) WITH (oids = false);
 
+TRUNCATE "cargos";
 INSERT INTO "cargos" ("uuid", "name", "english_name", "photo", "category", "price", "weight", "capacity", "description", "is_deleted") VALUES
-('c3ca454d-1bb7-479a-9092-aad63930c3eb',	'Кислород',	'oxygen',	'localhost:9000/images/c3ca454d-1bb7-479a-9092-aad63930c3eb.jpg',	'Кислород',	19500,	60,	0.05,	'Кислород в баллонах под высоким давлением',	'f'),
-('71a67173-bcb4-44ca-957d-84a71ed0cf81',	'Вода',	'water',	'localhost:9000/images/71a67173-bcb4-44ca-957d-84a71ed0cf81.jpg',	'Напитки',	110,	0.5,	0.0005,	'Вода родниковая очищенная',	'f'),
-('e4587acd-ea3d-4b3e-83a5-eef249cf90c3',	'Чай черный',	'tea',	'localhost:9000/images/e4587acd-ea3d-4b3e-83a5-eef249cf90c3.jpg',	'Напитки',	750,	0.003,	0.0003,	'Чай черный цейлонский без сахара в специализированном пакете',	'f'),
-('41ca2085-53f5-4de8-9ad0-636ceee31bfa',	'Хлеб пшеничный',	'bread',	'localhost:9000/images/41ca2085-53f5-4de8-9ad0-636ceee31bfa.jpg',	'Еда',	380,	0.03,	0.00015,	'Мука пшеничная, вода, маргарин, сахар, дрожжи, соль, молоко сухое',	'f'),
-('c2633eec-2766-4421-8513-b7deea2e025f',	'Каша гречневая с мясом',	'meat_and_grecha',	'localhost:9000/images/c2633eec-2766-4421-8513-b7deea2e025f.jpg',	'Еда',	15,	0.06,	0.00025,	'Крупа гречневая, соль, жир, фарш говяжий сушеный, лук сушеный, аромат говядины',	'f');
+('e4587acd-ea3d-4b3e-83a5-eef249cf90c3',	'Чай черный',	'tea',	'http://localhost:9000/images/e4587acd-ea3d-4b3e-83a5-eef249cf90c3.jpg',	'Напитки',	750,	0.003,	0.0003,	'Чай черный цейлонский без сахара в специализированном пакете',	'f'),
+('41ca2085-53f5-4de8-9ad0-636ceee31bfa',	'Хлеб пшеничный',	'bread',	'http://localhost:9000/images/41ca2085-53f5-4de8-9ad0-636ceee31bfa.jpg',	'Еда',	380,	0.03,	0.00015,	'Мука пшеничная, вода, маргарин, сахар, дрожжи, соль, молоко сухое',	'f'),
+('c2633eec-2766-4421-8513-b7deea2e025f',	'Каша гречневая с мясом',	'meat_and_grecha',	'http://localhost:9000/images/c2633eec-2766-4421-8513-b7deea2e025f.jpg',	'Еда',	15,	0.06,	0.00025,	'Крупа гречневая, соль, жир, фарш говяжий сушеный, лук сушеный, аромат говядины',	'f'),
+('c3ca454d-1bb7-479a-9092-aad63930c3eb',	'Кислород',	'oxygen',	'http://localhost:9000/images/c3ca454d-1bb7-479a-9092-aad63930c3eb.jpg',	'Кислород',	19500,	60,	0.05,	'Кислород в баллонах под высоким давлением',	'f'),
+('71a67173-bcb4-44ca-957d-84a71ed0cf81',	'Вода',	'water',	'http://localhost:9000/images/71a67173-bcb4-44ca-957d-84a71ed0cf81.jpg',	'Напитки',	110,	0.5,	0.0005,	'Вода родниковая очищенная',	'f');
 
 DROP TABLE IF EXISTS "flight_cargos";
 CREATE TABLE "public"."flight_cargos" (
@@ -32,11 +33,16 @@ CREATE TABLE "public"."flight_cargos" (
     CONSTRAINT "flight_cargos_pkey" PRIMARY KEY ("flight_id", "cargo_id")
 ) WITH (oids = false);
 
+TRUNCATE "flight_cargos";
 INSERT INTO "flight_cargos" ("flight_id", "cargo_id", "quantity") VALUES
-('0c89aa54-7452-471b-be0a-4101a11d5078',	'71a67173-bcb4-44ca-957d-84a71ed0cf81',	5),
 ('679e161d-76bf-4ddb-a4f1-94e386234bba',	'e4587acd-ea3d-4b3e-83a5-eef249cf90c3',	3),
 ('679e161d-76bf-4ddb-a4f1-94e386234bba',	'41ca2085-53f5-4de8-9ad0-636ceee31bfa',	6),
-('75d2da37-d6a8-4fd4-945f-ecb4c20d3075',	'c2633eec-2766-4421-8513-b7deea2e025f',	6);
+('47e6debd-287b-4b82-8f5d-9a49243b05ee',	'c2633eec-2766-4421-8513-b7deea2e025f',	6),
+('f65c9be2-e57e-4756-8b5f-845528687366',	'c2633eec-2766-4421-8513-b7deea2e025f',	4),
+('c1bccc9b-e8f0-4258-bd99-ccc8d7b6092c',	'c2633eec-2766-4421-8513-b7deea2e025f',	3),
+('2aef47f9-9e34-4651-8d1f-03653ac6bbae',	'e4587acd-ea3d-4b3e-83a5-eef249cf90c3',	1),
+('91f0d23b-2554-4077-b070-6905a5bd25fb',	'71a67173-bcb4-44ca-957d-84a71ed0cf81',	5),
+('91f0d23b-2554-4077-b070-6905a5bd25fb',	'41ca2085-53f5-4de8-9ad0-636ceee31bfa',	15);
 
 DROP TABLE IF EXISTS "flights";
 CREATE TABLE "public"."flights" (
@@ -52,10 +58,14 @@ CREATE TABLE "public"."flights" (
     CONSTRAINT "flights_pkey" PRIMARY KEY ("uuid")
 ) WITH (oids = false);
 
+TRUNCATE "flights";
 INSERT INTO "flights" ("uuid", "status", "creation_date", "formation_date", "completion_date", "customer_id", "moderator_id", "rocket_type", "shipment_status") VALUES
-('0c89aa54-7452-471b-be0a-4101a11d5078',	'завершен',	'2023-12-19 00:44:28.484434',	'2023-12-19 01:13:31.776652',	'2023-12-19 01:21:32.496799',	'efb6d99a-ebc4-4f61-a6a4-452cd0dfd286',	'620bc03b-c7ae-46a6-83c6-62f4a344f51b',	NULL,	'доставлено'),
 ('679e161d-76bf-4ddb-a4f1-94e386234bba',	'завершен',	'2023-12-19 00:53:34.078914',	'2023-12-19 01:14:51.343373',	'2023-12-19 02:31:41.428393',	'41336005-1f06-4762-b2ba-886537f9b679',	'620bc03b-c7ae-46a6-83c6-62f4a344f51b',	NULL,	'доставлено'),
-('75d2da37-d6a8-4fd4-945f-ecb4c20d3075',	'черновик',	'2023-12-19 02:41:19.221641',	NULL,	NULL,	'41336005-1f06-4762-b2ba-886537f9b679',	NULL,	NULL,	NULL);
+('47e6debd-287b-4b82-8f5d-9a49243b05ee',	'отклонен',	'2023-12-20 10:08:42.237641',	'2023-12-20 10:10:33.213088',	'2024-01-09 14:17:32.492943',	'efb6d99a-ebc4-4f61-a6a4-452cd0dfd286',	'620bc03b-c7ae-46a6-83c6-62f4a344f51b',	NULL,	'доставка отменена'),
+('c1bccc9b-e8f0-4258-bd99-ccc8d7b6092c',	'завершен',	'2024-01-09 15:52:18.423531',	'2024-01-09 15:52:39.705962',	'2024-01-09 15:53:36.195604',	'a860f543-eb88-40a6-b3ca-7c10167ed6db',	'620bc03b-c7ae-46a6-83c6-62f4a344f51b',	NULL,	'доставлено'),
+('2aef47f9-9e34-4651-8d1f-03653ac6bbae',	'завершен',	'2024-01-09 17:00:53.294132',	'2024-01-09 17:01:08.886835',	'2024-01-09 17:01:24.513879',	'41336005-1f06-4762-b2ba-886537f9b679',	'620bc03b-c7ae-46a6-83c6-62f4a344f51b',	NULL,	'доставлено'),
+('f65c9be2-e57e-4756-8b5f-845528687366',	'отклонен',	'2023-12-20 20:41:15.282014',	'2023-12-20 20:41:41.02188',	'2023-12-20 20:42:32.203602',	'41336005-1f06-4762-b2ba-886537f9b679',	'620bc03b-c7ae-46a6-83c6-62f4a344f51b',	NULL,	'доставка отменена'),
+('91f0d23b-2554-4077-b070-6905a5bd25fb',	'сформирован',	'2024-01-09 23:01:58.427172',	'2024-01-10 18:32:56.447858',	NULL,	'41336005-1f06-4762-b2ba-886537f9b679',	NULL,	'Союз',	'доставлено');
 
 DROP TABLE IF EXISTS "users";
 CREATE TABLE "public"."users" (
@@ -66,10 +76,12 @@ CREATE TABLE "public"."users" (
     CONSTRAINT "users_pkey" PRIMARY KEY ("uuid")
 ) WITH (oids = false);
 
+TRUNCATE "users";
 INSERT INTO "users" ("uuid", "role", "login", "password") VALUES
 ('efb6d99a-ebc4-4f61-a6a4-452cd0dfd286',	1,	'volodia',	'89749fe500cbbaba80324cab455e597c0ccbdf0a'),
 ('620bc03b-c7ae-46a6-83c6-62f4a344f51b',	2,	'admin',	'd033e22ae348aeb5660fc2140aec35850c4da997'),
-('41336005-1f06-4762-b2ba-886537f9b679',	1,	'roskosmos',	'd72368a923b71db34ebb60e5e2625ac9d4838ea4');
+('41336005-1f06-4762-b2ba-886537f9b679',	1,	'roskosmos',	'd72368a923b71db34ebb60e5e2625ac9d4838ea4'),
+('a860f543-eb88-40a6-b3ca-7c10167ed6db',	1,	'new',	'c2a6b03f190dfb2b4aa91f8af8d477a9bc3401dc');
 
 ALTER TABLE ONLY "public"."flight_cargos" ADD CONSTRAINT "fk_flight_cargos_cargo" FOREIGN KEY (cargo_id) REFERENCES cargos(uuid) NOT DEFERRABLE;
 ALTER TABLE ONLY "public"."flight_cargos" ADD CONSTRAINT "fk_flight_cargos_flight" FOREIGN KEY (flight_id) REFERENCES flights(uuid) NOT DEFERRABLE;
@@ -77,4 +89,4 @@ ALTER TABLE ONLY "public"."flight_cargos" ADD CONSTRAINT "fk_flight_cargos_fligh
 ALTER TABLE ONLY "public"."flights" ADD CONSTRAINT "fk_flights_customer" FOREIGN KEY (customer_id) REFERENCES users(uuid) NOT DEFERRABLE;
 ALTER TABLE ONLY "public"."flights" ADD CONSTRAINT "fk_flights_moderator" FOREIGN KEY (moderator_id) REFERENCES users(uuid) NOT DEFERRABLE;
 
--- 2023-12-19 02:43:06.149718+00
+-- 2024-01-10 20:19:12.532311+00
